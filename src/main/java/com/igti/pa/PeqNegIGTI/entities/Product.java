52c2fb69352541d2d9f7629e515e733851b4e70e
djdsfj <djdsfj@yahoo.com.br>
2020-09-14 21:04:59 -0300

    alterações nas entidades

--- src/main/java/com/igti/pa/PeqNegIGTI/entities/Product.java
+++ src/main/java/com/igti/pa/PeqNegIGTI/entities/Product.java
@@ -8,10 +8,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 
 
 @Entity
+@Table(name="TB_PRODUTO")
 public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
